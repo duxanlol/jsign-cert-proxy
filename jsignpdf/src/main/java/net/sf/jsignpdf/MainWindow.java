8227840127192ae6a8aaf6c8e40dd844c19cd7de
dusan <dusan.mandic@pmf.uns.ac.rs>
2022-08-04 13:03:05 +0200

    init window

---
+++ jsignpdf/src/main/java/net/sf/jsignpdf/MainWindow.java
@@ -0,0 +1,17 @@
+package net.sf.jsignpdf;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JPanel panel1;
+    private JList list1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JTextField textField1;
+    private JButton button4;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
